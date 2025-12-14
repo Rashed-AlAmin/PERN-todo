f6fa1981eaaf173ad0735c555d13b0acacc1785c
Rashed <rashedalamin69@gmail.com>
2025-12-14 14:00:47 +0600

    create a todo done

---
+++ server/db.js
@@ -0,0 +1,12 @@
+const Pool=require('pg').Pool
+require('dotenv').config()
+
+const pool=new Pool({
+    user:"postgres",
+    password:process.env.db_password,
+    host:"localhost",
+    port:5432,
+    database:process.env.db_name
+})
+
+module.exports=pool
--- server/index.js
+++ server/index.js
@@ -1,11 +1,33 @@
 const express=require('express')
 const app=express()
 const cors=require('cors')
+const pool=require('./db')
 
 //middleware
 app.use(cors())
-app.use(express())
+app.use(express.json()) //req.body
 
+//routes
+
+//create a todo
+app.post("/todos",async(req,res)=>{
+    try{
+        const {description}=req.body;
+          const newTodo=await pool.query(
+            "INSERT INTO todo (description) VALUES($1) RETURNING *"
+            ,[description])
+        res.json(newTodo.rows[0])
+    }catch(err){
+        console.error(err.message)
+    }
+})
+//get all todos
+
+//get a todo
+
+//update a todo
+
+//delete a todo
 
 app.listen(5000,()=>{
     console.log('the server is running on port 5000')
